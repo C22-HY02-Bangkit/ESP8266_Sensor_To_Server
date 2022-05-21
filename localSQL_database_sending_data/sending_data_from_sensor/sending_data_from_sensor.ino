f26ce53a9569343b2d4043025423304efd989481
BiggusMaximus <jahekeraton164@gmail.com>
2022-05-21 19:06:53 +0700

    Adding sending to SQL Local database

---
+++ localSQL_database_sending_data/sending_data_from_sensor/sending_data_from_sensor.ino
@@ -0,0 +1,85 @@
+// Adding sensor library and ESP library
+#include <DHT.h>
+#define DHTTYPE DHT22
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+//Pin for sensor
+#define soilPin 0
+#define DHTPIN D4
+
+
+// Adding wifi ssid, password and server name
+const char* ssid = "PARAMITA";
+const char* password = "76602223";
+const char* serverName = "http://192.168.0.101/hydroponic_bangkit/sensorToDatabase.php";
+
+
+int16_t soilValue;
+float humidity, temperature, heat_index;
+String keyPass_value = "HY02_companyBangkit";
+String sensorName = "DHT22";
+String sensorLocation = "Home";
+
+DHT dht(DHTPIN, DHTTYPE);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(soilPin, INPUT);
+  dht.begin();
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP()); 
+
+}
+
+void loop() {
+  // Read data from sensor
+  soilValue = analogRead(soilPin);
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+  heat_index = dht.computeHeatIndex(temperature, humidity);
+  
+  Serial.print("Humidity : " + String(humidity));
+  Serial.print("  | Temperature : " + String(temperature));
+  Serial.print("  | Heat index : " + String(heat_index) + "\n");
+
+  // If wifi is connected
+  if(WiFi.status() == WL_CONNECTED){
+    WiFiClient client;
+    HTTPClient http;
+
+    http.begin(client, serverName);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    String httpRequestData = "keyPass=" + keyPass_value + "&sensor=" + sensorName
+                          + "&location=" + sensorLocation + "&value1=" + String(humidity)
+                          + "&value2=" + String(temperature) + "&value3=" + String(heat_index) + "";
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+    
+  // Send HTTP POST request
+    int httpResponseCode = http.POST(httpRequestData);
+
+    if (httpResponseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+  delay(1000);
+}
